8dcb34fcb4974f474c43840ee1eb6f7150b282c3
AKS <abhishekksva@gmail.com>
2026-03-28 04:20:51 +0530

    Added Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import torch
+torchvision
+import torchvision
+import torchvision.transforms as transforms
+st.title("CNN CIFAR10 - Image Classifier")
+st.write("Model trainedon CIFAR10 dataset")
+st.write("Test Accuracy: 74.71%)
