08907fe5b34a2a8df9e8bce6d1758938cff6c2ea
Abhishek Krishna Srivastava <99735131+abhishekksva@users.noreply.github.com>
2026-03-29 02:47:19 +0530

    app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import torchvision
 import torchvision.transforms as transforms
 st.title("CNN CIFAR10 - Image Classifier")
 st.write("Model trainedon CIFAR10 dataset")
-st.write("Test Accuracy: 74.71%)
+st.write("Test Accuracy: 74.71%")
